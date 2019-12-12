2659b0628ea46852022dda94e5306a7bd5a839bb
IKEAPFU <37018828+SIRICHANDANA2597@users.noreply.github.com>
2019-12-12 18:12:23 +0530

    Update index_back.js

--- CloudNodeJS/index_back.js
+++ CloudNodeJS/index_back.js
@@ -1,6 +1,6 @@
 //const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
 //const { IamAuthenticator } = require('ibm-watson/auth');
-const express = require('express');
+//const express = require('express');
 //const app = express();
 
 //const speechToText = new SpeechToTextV1({
@@ -14,19 +14,19 @@ const express = require('express');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'mysql.database-check.svc.cluster.local',
+  host: 'custom-mysql.gamification.svc.cluster.local',
   port     : '3306',
   user     : 'xxuser',
   password : 'welcome1',
   database : 'sampledb'
-});
+})
 
 connection.connect();
 
 connection.query('SELECT * from XXIBM_PRODUCT_CATALOGUE', function (err, rows, fields) {
   if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
+  console.log('The solution is: ', rows);
 });
 
 connection.end();
