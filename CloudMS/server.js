5ba3d4fe251a2f153eb8940bff610833cfb035dc
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2019-12-13 14:46:01 +0530

    Update server.js

--- CloudMS/server.js
+++ CloudMS/server.js
@@ -3,7 +3,8 @@ const app = express();
 //const port = process.env.PORT || 3000;
 
 const routes = require('./api/routes');
-routes(app);
+//routes(app);
+app.use('/product', routes);
 //app.listen(port, function() {
   // console.log('Server started on port: ' + port);
 //});
