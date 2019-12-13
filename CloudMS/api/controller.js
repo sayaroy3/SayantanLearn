6f25512594f5e985c88d0c58b0281c4b4582c639
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2019-12-13 14:52:13 +0530

    Update controller.js

--- CloudMS/api/controller.js
+++ CloudMS/api/controller.js
@@ -2,7 +2,7 @@
 
 var properties = require('../package.json');
 var products = require('../service/Product');
-
+console.log(4);
 var controllers = {
    getProducts: function(req, res) {
     products.find(req, res, function(err, productlist) {
