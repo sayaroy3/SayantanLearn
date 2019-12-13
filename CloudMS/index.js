ab0a907bf7b7f9fe9cc25dbda2ad73478474a682
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2019-12-13 14:44:26 +0530

    Update index.js

--- CloudMS/index.js
+++ CloudMS/index.js
@@ -4,7 +4,8 @@ const app = express();
 //const port = process.env.PORT || 3000;
 
 const routes = require('./api/routes');
-routes(app);
+app.use('/product', routes);
+//routes(app);
 //app.listen(port, function() {
   // console.log('Server started on port: ' + port);
 //})
