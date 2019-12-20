399fcc887efb832ee3e9fde1843669f32fad64a1
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2019-12-20 11:25:20 +0530

    Update Product.js

--- CloudMS/service/Product.js
+++ CloudMS/service/Product.js
@@ -17,7 +17,7 @@ var Products = {
        connection.connect(); 
        connection.query('Select c.ITEM_NUMBER,c.DESCRIPTION,c.LONG_DESCRIPTION,c.LIST_PRICE,c.DISCOUNT,c.SKU_UNIT_OF_MEASURE,d.BRAND,c.SKU_ATTRIBUTE_VALUE1,c.SKU_ATTRIBUTE_VALUE2,c.IN_STOCK from(select A.ITEM_NUMBER,B.DESCRIPTION,B.LONG_DESCRIPTION,A.LIST_PRICE,A.IN_STOCK,A.DISCOUNT,B.SKU_UNIT_OF_MEASURE,B.SKU_ATTRIBUTE_VALUE1,B.SKU_ATTRIBUTE_VALUE2 from XXIBM_PRODUCT_PRICING A JOIN XXIBM_PRODUCT_SKU B ON A.ITEM_NUMBER = B.ITEM_NUMBER) c JOIN  XXIBM_PRODUCT_STYLE d ON c.DESCRIPTION = d.DESCRIPTION', function (err, rows, fields) {
          if (err) throw err;
-         res.send("{""items"": [" + rows  + "]}");     
+         res.send('{"items": [' +  rows  + ']}');     
          //console.log('The solution is: ', rows);
        });
        
