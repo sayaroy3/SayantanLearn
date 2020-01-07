aa38294c9ff16ec8d250aaf3b20f07248fe0cc14
Siri <siri.chandu2597@gmail.com>
2020-01-07 16:43:16 +0530

    django-ex

---
+++ django-ex/imagePicker/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ImagepickerConfig(AppConfig):
+    name = 'imagePicker'
---
+++ django-ex/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sirich~1\desktop\contai~1\newfol~1\django~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
