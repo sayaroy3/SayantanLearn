b6f4177eb730a3ae9ca2b133d157a6b9eb70eb8a
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2020-01-20 14:24:41 +0530

    Update controller.js

--- CloudMSNew/api/controller.js
+++ CloudMSNew/api/controller.js
@@ -5,7 +5,8 @@ var products = require('../service/Product');
 console.log(4);
 var controllers = {
    getProducts: function(req, res) {
-    products.find(req, res, function(err, productlist) {
+    var newvar = req.params.pid;
+    products.find(req, res, newvar, function(err, productlist) {
                if (err)
                    res.send(err);
                res.json(productlist);
