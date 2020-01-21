83aa6aa96b279d55a164e02d087e6fac9516abbe
sayaroy3 <31437385+sayaroy3@users.noreply.github.com>
2020-01-21 17:49:37 +0530

    Update index.js
    Updated with removing comment out sections

--- CloudMSNew/index.js
+++ CloudMSNew/index.js
@@ -1,12 +1,7 @@
 
 const express = require('express');
 const app = express();
-//const port = process.env.PORT || 3000;
 
 const routes = require('./api/routes');
-console.log(2);
 app.use('/product', routes);
-//routes(app);
-//app.listen(port, function() {
-  // console.log('Server started on port: ' + port);
-//})
+
