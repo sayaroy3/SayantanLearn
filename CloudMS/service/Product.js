38fce89672e9825bb3806e3d606bdd525d97fdb6
Siri <siri.chandu2597@gmail.com>
2020-06-22 21:57:19 +0530

    CC

--- CloudMS/service/Product.js
+++ CloudMS/service/Product.js
@@ -1,32 +1,46 @@
 var request = require('request');
 var express       = require('express');
-var pg            = require('pg');
-var config = {
+const { Pool, Client } = require('pg');
+
+
+const pool = new Pool({
   user: 'admin',
   database: 'sampledb',
-  host: 'localhost',
+  host: '127.0.0.1',
   password: 'admin123',
   port: 5432
-};
-
-console.log("config : "+JSON.stringify(config));
-var pool = new pg.Pool(config);
-pool.query('SELECT NOW()', function(err, res) {
-  if (err) throw err
-  console.log(res.rows)
 })
 
+//pool.query('SELECT NOW()', (err, res) => {
+ // console.log(err, res)
+ // pool.end()
+//})
+
+const client = new Client({
+  user: 'admin',
+  database: 'sampledb',
+  host: '127.0.0.1',
+  password: 'admin123',
+  port: 5432
+})
+client.connect()
+
+//client.query('SELECT NOW()', (err, res) => {
+ // console.log(err, res)
+ // client.end()
+//})
 
 var Products = {
-   find: function(req, res, next) {
-       connection.query('Select c.ITEM_NUMBER,c.DESCRIPTION,c.LONG_DESCRIPTION,c.LIST_PRICE,c.DISCOUNT,c.SKU_UNIT_OF_MEASURE,d.BRAND,c.SKU_ATTRIBUTE_VALUE1,c.SKU_ATTRIBUTE_VALUE2,c.IN_STOCK from(select A.ITEM_NUMBER,B.DESCRIPTION,B.LONG_DESCRIPTION,A.LIST_PRICE,A.IN_STOCK,A.DISCOUNT,B.SKU_UNIT_OF_MEASURE,B.SKU_ATTRIBUTE_VALUE1,B.SKU_ATTRIBUTE_VALUE2 from XXIBM_PRODUCT_PRICING A JOIN XXIBM_PRODUCT_SKU B ON A.ITEM_NUMBER = B.ITEM_NUMBER) c JOIN  XXIBM_PRODUCT_STYLE d ON c.DESCRIPTION = d.DESCRIPTION', function(err,rows,fields){;
-        if (err) throw err;
-        var rows = JSON.stringify(rows);
-         rows = '{"data":' + rows + '}';
-         res.setHeader('Access-Control-Allow-Origin', '*');
-         res.send(rows);             
-      });
-   }
+  find: function(req, res, next) {
+      client.query('Select a.Hotel,b.Places,b.Description,b.VisitingPlaces,a.PPN from Hotel a join Travel_Places b ON a.Place = b.slno', 
+      function(err,results){
+       if (err) throw err;
+       var rows = JSON.stringify(results.rows);
+        rows = '{"data":' + rows + '}';
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send(rows);             
+     });
+  }
 };
 
 module.exports = Products;
